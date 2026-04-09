b6da0e73979e73d9cce65e34ee91fefb621d13e8
djdes <djdes@users.noreply.github.com>
2026-04-09 07:25:59 +0300

    Disable app shell caching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ServiceWorkerRegister } from "@/components/layout/sw-register";
 import "./globals.css";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
